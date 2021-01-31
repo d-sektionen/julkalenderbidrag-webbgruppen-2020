63546e35194ad06468a03601f6db598a77150977
Max <maxskan98@gmail.com>
2021-01-31 12:15:57 +0100

    replaced all files with the ones from the django repo

--- test.js
+++
@@ -1,9 +0,0 @@
-result = 0;
-score = 372;
-score -= (Math.floor(score / 10));
-for (let index = 0; index < score; index++) {
-    interval = 1000 -3 * index;
-    result += interval
-    
-}
-console.log(score,result*0.95, interval)
